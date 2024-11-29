8ffeb8ba78eb8849307dc46605a8078219bb005b
abirgharbi <abirgharbi046@gmail.com>
2024-11-29 14:34:47 +0100

    Deposer Sujet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import dbConnection from './dbConfig/dbConnection.js';
+import routerPFA from './routes/PFARoutes.js';
 
 dotenv.config();
 
@@ -20,3 +21,10 @@ app.use(cors());
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/",routerPFA)
+
+export default app;
---
+++ routes/PFARoutes.js
@@ -0,0 +1,21 @@
+// add PFA routes
+import { createSubjects, getSubjects, updateSubject, deleteSubject } from "../controllers/PFAController.js";
+import express from "express";
+
+const router = express.Router();
+
+
+
+// Route to create multiple subjects
+router.post('/PFA/post', createSubjects);
+
+// Route to get all subjects
+router.get("/PFA/mine", getSubjects);
+
+// Route to update a subject
+router.put('/PFA/:id', updateSubject);
+
+// Route to delete a subject
+router.delete('/PFA/:id', deleteSubject);
+
+export default router;
