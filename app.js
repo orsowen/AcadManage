69443b727aa26f702b017adbd37cfe9c11ba5c66
OneEyeX <chedly199@gmail.com>
2024-12-02 15:59:05 +0100

    cleaned app.js code

--- app.js
+++ app.js
@@ -17,17 +17,16 @@ import UserConnexionRoutes from "./routes/UserConnexionRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(express.json()); // To parse JSON data in POST requests
 const PORT = process.env.PORT || 8800;
 
 // MongoDB Connection
 dbConnection();
-app.use(express.json());
+
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // To parse JSON data in POST requests
 
-
+// ROUTES
 app.use("/pfe", DepositPeriod);
 app.use(["/PFE", "/PFA", "/STAGE"], DepositPeriod);
 app.use("/internships", soutenanceStageRoutes);
@@ -37,16 +36,12 @@ app.use("/topics", topicsRoutes);
 app.use("/users", UserConnexionRoutes);
 app.use('/teachers', teacherRoutes);
 app.use("/students", studentRoutes);
+app.use("/", routerPFA);
 
 
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/", routerPFA);
 
 export default app;
