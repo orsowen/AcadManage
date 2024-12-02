ea1e6dc4944b538e70793f0add942cd7524da6b0
orsowen <104215505+orsowen@users.noreply.github.com>
2024-12-02 15:37:08 +0100

    Revert "Gestion pfe (#24)"
    This reverts commit 1f6606b40e8020db547562912db30f1227902084.

--- controllers/DepositPeriod.js
+++ controllers/DepositPeriod.js
@@ -132,6 +132,7 @@ export const updateDepositPeriod = async (req, res) => {
       { Start_Deposit, End_Deposit, Start_Choice, End_Choice },
       { new: true }
     );
+    console.log(choix);
     if (!updatedPeriod) {
       return res.status(404).json({ message: `Aucune période trouvée pour ${choix}.` });
     }
