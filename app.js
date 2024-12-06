93dd0cbd98a00ed40a9821a2d34998ec4d03b40e
OneEyeX <chedly199@gmail.com>
2024-12-06 10:36:33 +0100

    fixed main server : removed duplicated declarations | cleaned code

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 
 import cors from "cors";
 import dotenv from "dotenv";
-import routerPFA from "./routes/PFARoutes.js";
 import express from "express";
 import dbConnection from "./dbConfig/dbConnection.js";
 import DepositPeriod from "./routes/DepositPeriod.js";
 import internshipRoutes from "./routes/InternshipRoutes.js";
+import routerPFA from "./routes/PFARoutes.js";
+import PFEInternship from "./routes/PFEInternship.js";
 import soutenanceStageRoutes from "./routes/SoutenanceStageRoutes.js";
 import studentRoutes from "./routes/StudentRoutes.js";
 import teacherRoutes from "./routes/teacherRoutes.js";
-import topicsRoutes from "./routes/topicRoutes.js";
 import UserConnexionRoutes from "./routes/UserConnexionRoutes.js";
-import PFEInternship from "./routes/PFEInternship.js"
-
 
 // FOR TESTING ONLY (DO NOT DELETE)
 import topicsRoutes from './routes/topicRoutes.js';
@@ -28,16 +26,12 @@ const PORT = process.env.PORT || 8800;
 
 // MongoDB Connection
 dbConnection();
-app.use(express.json());
 // Middleware
 app.use(cors());
-
 app.use(express.json());
 
 
 // ROUTES
-
-
 app.use(["/PFE", "/PFA", "/STAGE"], DepositPeriod);
 app.use("/PFE", PFEInternship);
 app.use("/internships", soutenanceStageRoutes);
@@ -54,11 +48,5 @@ app.use("/topics", topicsRoutes);
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/", routerPFA);
 
 export default app;
