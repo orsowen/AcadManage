6fb14fc08249699c54bb188594c9e95cc59fdb9c
orsowen <104215505+orsowen@users.noreply.github.com>
2024-12-08 21:23:34 +0000

    Gestion pfe (#33)
    * Add PFE sujet topic
    
    * create pfe (controller,route)
    
    * fixing creation pfe
    
    * Add update sujet in the period
    
    * pfe sujet work well
    
    * pfe sujet work well
    
    * pfe sujet work well
    
    * Change the model
    
    * Change the model
    
    * FIXING SOME PROBLEM
    
    * add lister sujet et choisir plusieur sujet enseignant
    
    * delete some files
    
    * Refactorisation PFE controller : list pfe info statut de soutenance

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ import dbConnection from "./dbConfig/dbConnection.js";
 import DepositPeriod from "./routes/DepositPeriod.js";
 import internshipRoutes from "./routes/InternshipRoutes.js";
 import routerPFA from "./routes/PFARoutes.js";
+
 import PFE from "./routes/PFE.js";
 import planningStageRoutes from "./routes/PlanningStageRoutes.js";
 import studentRoutes from "./routes/StudentRoutes.js";
 import teacherRoutes from "./routes/teacherRoutes.js";
 import UserConnexionRoutes from "./routes/UserConnexionRoutes.js";
-
+import PFE from "./routes/PFE.js"
 
 
 // ENVIRONMENT variables configuration
@@ -32,6 +33,7 @@ app.use(express.json());
 
 app.use(["/PFE", "/PFA", "/STAGE"], DepositPeriod);
 app.use("/PFE", PFE);
+
 app.use("/internships", planningStageRoutes);
 app.use("/internships", internshipRoutes);
 app.use("/users", UserConnexionRoutes);
