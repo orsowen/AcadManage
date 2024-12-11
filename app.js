167627f175f914e730424b0b818cd0cfa4495682
Rania Hammami <77574506+Rania-hammami@users.noreply.github.com>
2024-12-11 01:40:17 +0000

    changed router to index file

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ import dbConnection from "./dbConfig/dbConnection.js";
 import DepositPeriod from "./routes/DepositPeriod.js";
 import router from './routes/index.js';
 import routerPFA from "./routes/PFARoutes.js";
-
 import PFE from "./routes/PFE.js";
 import internshipRoutes from './routes/InternshipRoutes.js';
 import UserConnexionRoutes from "./routes/UserConnexionRoutes.js"
-import skillRoutes from "./routes/skill.js";
-import subjectRoutes from "./routes/subject.js";
-import testNotificationsRouter from './routes/test_notif.js';
 
 // ENVIRONMENT variables configuration
 dotenv.config();
@@ -29,22 +25,19 @@ app.use(express.json());
 // ROUTES
 
 app.use(["/PFE", "/PFA", "/Internership"], DepositPeriod);
-app.use('/test-notifications', testNotificationsRouter);
-app.use("/competences", skillRoutes);
-app.use("/matieres", subjectRoutes);
 app.use("/pfe", DepositPeriod);
 app.use(["/PFE", "/PFA", "/STAGE"], DepositPeriod);
 app.use("/PFE", PFE);
 app.use("/", routerPFA);
+app.use("/internships", internshipRoutes);
+app.use("/users", UserConnexionRoutes);
+
+
 app.use(router); // Place router here for better organization
 
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-// Additional routes
-app.use("/internships", internshipRoutes);
-app.use("/users", UserConnexionRoutes);
 
 export default app;
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ import studentRoutes from "./StudentRoutes.js";
 import teacherRoutes from "./TeacherRoutes.js";
 import UserConnexionRoutes from "./UserConnexionRoutes.js";
 import PFE from "./PFE.js";
+import skillRoutes from "./skill.js";
+import subjectRoutes from "./subject.js";
+import testNotificationsRouter from './test_notif.js';
 
 const router = express.Router();
 
@@ -17,7 +20,9 @@ router.use("/users", UserConnexionRoutes);
 router.use("/teachers", teacherRoutes);
 router.use("/students", studentRoutes);
 router.use("/PFE", PFE);
-
+router.use('/test-notifications', testNotificationsRouter);
+router.use("/competences", skillRoutes);
+router.use("/matieres", subjectRoutes);
 // Hot l route mta3ek lenna fi 3oudh fi App.js
 // esstaamel router.use() mouch app.use() w kahaw mriguel
 
