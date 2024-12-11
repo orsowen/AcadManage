4de1543e75004a5cdd6a26a5ae55af94199aa354
Rania Hammami <77574506+Rania-hammami@users.noreply.github.com>
2024-12-11 17:18:46 +0000

    get subjects by teacher

--- models/Subject.js
+++ models/Subject.js
@@ -55,12 +55,8 @@ const SubjectSchema = new mongoose.Schema({
     teachers: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Teacher', // Référence au modèle User
-<<<<<<< HEAD
         default: null,
-}],
-=======
     }],
->>>>>>> origin/main
     
     students: [{  // Tableau d'ID d'étudiants
         type: mongoose.Schema.Types.ObjectId,
