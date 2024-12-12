1cbece86a580c31aa877994600aea39bb12b33a6
orsowen <orsowenbenabdallah5@gmail.com>
2024-12-12 17:05:27 +0100

    fixin app.js

--- routes/StudentRoutes.js
+++ routes/StudentRoutes.js
@@ -1,24 +1,18 @@
 import express from 'express';
 import {
+    createCV,
     createStudent,
     deleteStudent,
     getAllStudents,
     getStudentById,
     getStudentProfile,
     updateStudent,
-<<<<<<< HEAD
-    updateStudentPassword,
-    updateStudentProfile,
-    createCV,
-=======
     updateStudentProfile
->>>>>>> origin/main
 } from '../controllers/StudentController.js';
 import { toggleArchiveUser, updatePassword } from '../controllers/UserController.js';
 import { isAdmin, isAdminOrTeacher, isStudent } from "../middlewares/authentication.js";
-
-
 const router = express.Router();
+
 
 // get Own profile for student
 router.get('/me', isStudent, getStudentProfile);
@@ -28,20 +22,6 @@ router.post('/', createStudent);
 
 // Get all students
 router.get('/', isAdminOrTeacher, getAllStudents);
-<<<<<<< HEAD
-// GET /students/:id - Get a student by ID
-router.get('/:id', isAdmin, getStudentById);
-// ( get dont work thats why post)
-router.post('/profile', isStudent, getStudentProfile);
-// PATCH /students/:id - Update a student by ID
-router.patch('/:id', isAdmin, updateStudent);
-// update own profile
-router.put('/me', isStudent, updateStudentProfile);
-
-// patch update student password
-router.patch('/:id/password', isAdmin, updateStudentPassword);
-// DELETE /students/:id - Delete a student by ID
-=======
 
 // Get a student by ID
 router.get('/:id', isAdmin, getStudentById);
@@ -56,7 +36,6 @@ router.put('/me', isStudent, updateStudentProfile);
 router.patch('/:id/password', isAdmin, updatePassword("student"));
 
 // Delete a student by ID
->>>>>>> origin/main
 router.delete('/:id', isAdmin, deleteStudent);
 
 // Archive a teacher
--- routes/TeacherRoutes.js
+++ routes/TeacherRoutes.js
@@ -1,55 +1,24 @@
-<<<<<<< HEAD
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import dbConnection from "./dbConfig/dbConnection.js";
-import router from './routes/index.js';
-import routerPFA from "./routes/PFARoutes.js";
-=======
-import express from 'express';
 import {
-    createTeacher,
-    deleteTeacher,
-    getAllTeachers,
-    getTeacherById,
-    getTeacherProfile,
-    updateTeacher,
-    updateTeacherByToken
+  createTeacher,
+  deleteTeacher,
+  getAllTeachers,
+  getTeacherById,
+  getTeacherProfile,
+  updateTeacher,
+  updateTeacherByToken
 } from '../controllers/TeacherController.js';
 import { toggleArchiveUser, updatePassword } from '../controllers/UserController.js';
 import { isAdmin, isTeacher } from "../middlewares/authentication.js";
->>>>>>> origin/main
+import express from 'express';
 
-// ENVIRONMENT variables configuration
-dotenv.config();
+const router = express.Router();
 
-// MongoDB Connection
-dbConnection();
 
-<<<<<<< HEAD
-const app = express();
-const PORT = process.env.PORT || 8800;
-=======
 // get Own Profile 
 router.get('/me', isTeacher, getTeacherProfile);
->>>>>>> origin/main
 
-// Middleware
-app.use(cors());
-app.use(express.json());
 
-// ROUTES
-app.use("/", routerPFA);
-app.use(router); // Place router in index.js fi 3oudh lenna for better organization
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
-<<<<<<< HEAD
-export default app;
-=======
 // Update a teacher
 router.patch('/:id', isAdmin, updateTeacher);
 
@@ -66,4 +35,3 @@ router.delete('/:id', isAdmin, deleteTeacher);
 router.put('/:id', isAdmin, toggleArchiveUser("teacher")); // Route to Archive user
 
 export default router;
->>>>>>> origin/main
