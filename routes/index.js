655648368f8c98b9a06c71ceb9322f440243645c
Chedly CHAHED <56316545+OneEyeX@users.noreply.github.com>
2025-01-03 23:03:22 +0100

    Gestion stages: STAGE ARCHIVE function (#95)
    * improved bulk archive for STAGE: added archive PlaningStage Option
    * added a route to test STAGE ARCHIVE

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 import express from "express";
+import { archiveInternshipsByYear } from "../controllers/internshipController.js";
 import { notifyAboutDepositDeadline } from "../utils/LateNotifReminder.js";
 import choicePFA from "./Choice.js";
+import DefensePFE from "./DefensePFE.js";
 import DepositPeriod from "./DepositPeriod.js";
 import internshipRoutes from "./InternshipRoutes.js";
 import routerPFA from "./PFA.js";
 import PFE from "./PFE.js";
 import planningStageRoutes from "./PlanningStageRoutes.js";
+import yearsRoutes from "./SeasonRoutes.js";
 import skillRoutes from "./skill.js";
 import studentRoutes from "./StudentRoutes.js";
 import subjectRoutes from "./subject.js";
 import teacherRoutes from "./TeacherRoutes.js";
-import yearsRoutes from "./SeasonRoutes.js";
 import testNotificationsRouter from "./test_notif.js";
 import UserConnexionRoutes from "./UserConnexionRoutes.js";
-import DefensePFE from "./DefensePFE.js";
 const router = express.Router();
 
 // ROUTES --------------------------------
@@ -31,6 +32,8 @@ router.use("/matieres", subjectRoutes);
 router.use("/PFA", routerPFA);
 router.use("/PFAChoices", choicePFA);
 router.use("/years", yearsRoutes);
+// test internship archive
+router.post('/internships/testArcvhive', archiveInternshipsByYear("2024-2025", true));
 
 // TEST MAIL NOTIFICATION
 router.get("/test-notif-late-depot-satge", async (req, res) => {
